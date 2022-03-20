414e8dfb9d4c3030176f3536a6ae7e69443e1031
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-03-20 15:26:41 -0500

    error msg on workshop

--- scripts/workshopEthers.js
+++ scripts/workshopEthers.js
@@ -104,7 +104,14 @@ const addListing = async() => {
         }
     }
     catch (error) {
-        window.alert(error);
+        if ((error.message).includes("You are not Authorized for this ERC20 Contract!")) {
+            await displayErrorMessage(`You are not authorized to add listings for $CHEETH!`);
+        }
+        else {
+            await displayErrorMessage("An error occurred. See console and window alert for details...")
+            window.alert(error);
+            console.log(error);
+        }
     }
 }
 
